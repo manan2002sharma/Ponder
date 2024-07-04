03decc41c0845e9f8100dd758fdc651ea4536a9a
manan2002sharma <109142063+manan2002sharma@users.noreply.github.com>
2024-07-04 20:40:05 +0530

    first commit

---
+++ src/components/Quote.tsx
@@ -0,0 +1,19 @@
+
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-3xl font-bold">
+                    "This project is developed for people to share thoughts freely"
+                </div>
+                <div className="max-w-md text-xl font-semibold text-left mt-4">
+                    Developed by - Manan Sharma
+                </div>
+                <div className="max-w-md text-sm font-light text-slate-400">
+                    Student
+                </div>
+            </div>
+        </div>
+        
+    </div>
+}
